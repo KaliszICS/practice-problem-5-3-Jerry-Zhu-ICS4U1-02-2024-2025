6ecc2c5dc6209c08b183c02b0a9ec6ccd057762d
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-05-15 01:49:23 +0000

    finish practice problems

---
+++ src/main/java/Animal.java
@@ -0,0 +1,39 @@
+abstract class Animal {
+    protected String name; 
+    protected int age;
+    protected String type;
+    protected int position;
+
+    public Animal(String name, int age, String type, int position){
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.position = position;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public String sleep(){
+        return "Zzz";
+    }
+
+    abstract void move();
+
+    abstract String animalSound();
+
+    
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat extends Animal{
+    
+    public Cat(String name, int age, String type, int position){
+        super(name, age, type, position);
+    }
+
+    @Override
+    public void move(){
+        this.position += 5;
+    }
+
+    @Override
+    public String animalSound(){
+        return "Meow";
+    }
+}
